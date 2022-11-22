117d4ba57f1f9a00d9ab4621d23a335301744662
Diego Flores <floresdiego2001xd@gmail.com>
2022-11-22 17:55:53 -0500

    Agregar archivos de proyecto.

---
+++ LocalDatabase/LocalDatabase/Models/Artista.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalDatabase.Models
+{
+    public class Artista
+    {
+        public int ArtistaID { set; get; }
+        public string Nombre { set; get; }
+    }
+}
